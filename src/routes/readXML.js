5e3182045c33e5568ad37be330600763b45b153f
nkaw981 <nkaw981@aucklanduni.ac.nz>
2026-09-16 17:26:51 +1200

    Update readXML.js

--- src/routes/readXML.js
+++ src/routes/readXML.js
@@ -1,16 +1,12 @@
 import { DOMParser } from 'xmldom'
 
 export default async function() {
-    console.log("Trying to fetch")
     let response = await fetch('https://blog.neurosama.com/feed.xml');
     if (response.status != 200) {
         return `${response.status}, ${response.statusText}`
     } 
 
     const parser = new DOMParser();
-    console.log("Trying...")
-    let domData = parser.parseFromString(response.body, "application/xml");
-    console.log("Success")
-    console.log(domData)
+    let domData = parser.parseFromString(response.body, "text/xml");
     return domData;
 }
