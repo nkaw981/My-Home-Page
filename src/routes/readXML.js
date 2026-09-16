70828f5bbac17d33c593070a91f9152bc91f600d
nkaw981 <nkaw981@aucklanduni.ac.nz>
2026-09-17 00:01:57 +1200

    Update readXML.js

--- src/routes/readXML.js
+++ src/routes/readXML.js
@@ -13,5 +13,5 @@ export default async function() {
 
     const parser = new DOMParser();
     let domData = parser.parseFromString(await response.text(), "text/xml");
-    return `${typeof domData.children}\n${domData.children}\n----\n${domData}`;
+    return `${typeof domData.childNodes.item(1)}\n${domData.childNodes.item(1)}\n----\n${domData}`;
 }
