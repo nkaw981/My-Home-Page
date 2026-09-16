731b59609c4e12404cbdbee8dd879a401bc5cd41
nkaw981 <nkaw981@aucklanduni.ac.nz>
2026-09-16 16:42:34 +1200

    Update readXML.js

--- src/routes/readXML.js
+++ src/routes/readXML.js
@@ -4,5 +4,6 @@ export default async function() {
     let xmlData = (await fetch('https://blog.neurosama.com/feed.xml')).body;
     const parser = new DOMParser();
     let domData = parser.parseFromString(xmlData, "application/xml");
-    return domData.nodeName;
+    console.log(domData)
+    return domData;
 }
